9113c87c54a388f0999e3572aa60389fbd182df9
wenyanhe-sjsu <laksmikameswari.nishthala@sjsu.edu>
2019-04-24 09:12:43 -0700

    sensor test data onging

--- SmartAgCloud/Backend/test_data/sensor_test_data.js
+++ SmartAgCloud/Backend/test_data/sensor_test_data.js
@@ -109,4 +109,114 @@
         value: 15.87
     },
 },
+{
+    sensorType: "temperature",
+    status: "on",
+    x_coordinate: 3,
+    y_coordinate: 5,
+    data: {
+        timestamp: ISODate("2018-06-30T15:00:01Z"),
+        // type: "temperature",
+        value: 0.83
+    },
+},
+{
+    sensorType: "temperature",
+    status: "on",
+    x_coordinate: 7,
+    y_coordinate: 17,
+    data: {
+        timestamp: ISODate("2018-06-30T16:00:01Z"),
+        // type: "temperature",
+        value: 4.12
+    },
+},
+{
+    sensorType: "temperature",
+    status: "off",
+    x_coordinate: 31,
+    y_coordinate: 79,
+    data: {
+        timestamp: ISODate("2018-06-30T18:00:01Z"),
+        // type: "temperature",
+        value: null
+    },
+},
+{
+    sensorType: "temperature",
+    status: "maintenance",
+    x_coordinate: 12,
+    y_coordinate: 2,
+    data: {
+        timestamp: ISODate("2018-06-30T21:00:01Z"),
+        // type: "temperature",
+        value: null
+    },
+},
+{
+    sensorType: "temperature",
+    status: "on",
+    x_coordinate: 45,
+    y_coordinate: 23,
+    data: {
+        timestamp: ISODate("2018-06-30T22:00:01Z"),
+        // type: "temperature",
+        value: 19.68
+    },
+},
+{
+    sensorType: "temperature",
+    status: "on",
+    x_coordinate: 123,
+    y_coordinate: 22,
+    data: {
+        timestamp: ISODate("2018-07-01T01:00:01Z"),
+        // type: "temperature",
+        value: 4.26
+    },
+},
+{
+    sensorType: "temperature",
+    status: "off",
+    x_coordinate: 452,
+    y_coordinate: 97,
+    data: {
+        timestamp: ISODate("2018-07-01T03:00:01Z"),
+        // type: "temperature",
+        value: null
+    },
+},
+{
+    sensorType: "temperature",
+    status: "on",
+    x_coordinate: 34,
+    y_coordinate: 473,
+    data: {
+        timestamp: ISODate("2018-07-01T07:00:01Z"),
+        // type: "temperature",
+        value: 11.93
+    },
+},
+{
+    sensorType: "temperature",
+    status: "on",
+    x_coordinate: 62,
+    y_coordinate: 59,
+    data: {
+        timestamp: ISODate("2018-07-01T05:00:01Z"),
+        // type: "temperature",
+        value: 13.48
+    },
+},
+{
+    sensorType: "temperature",
+    status: "maintenance",
+    x_coordinate: 432,
+    y_coordinate: 781,
+    data: {
+        timestamp: ISODate("2018-07-01T10:00:01Z"),
+        // type: "temperature",
+        value: null
+    },
+},
 ]
