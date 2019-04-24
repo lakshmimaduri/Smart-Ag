504ae1c76eaf17310ebdd340c74e5946c560aa6c
wenyanhe-sjsu <laksmikameswari.nishthala@sjsu.edu>
2019-04-24 03:10:40 -0700

    user test data done

--- SmartAgCloud/Backend/test_data/user_test_data.js
+++ SmartAgCloud/Backend/test_data/user_test_data.js
@@ -1,7 +1,8 @@
-[{
+[
+{
     username: "user1",
     password: "pass1" ,
-    email: "user1@smartag",
+    email: "user1@smartag.com",
     userType: "farmer",
     firstName: "user1",
     lastName: "lastname1",
@@ -10,5 +11,102 @@
     county: "county1"
 },
 {
-    
-}]
+    username: "user2",
+    password: "pass2" ,
+    email: "user2@smartag.com",
+    userType: "farmer",
+    firstName: "user2",
+    lastName: "lastname2",
+    city: "city2",
+    state: "state2",
+    county: "county2"
+},
+{
+    username: "user3",
+    password: "pass3" ,
+    email: "user3@smartag.com",
+    userType: "farmer",
+    firstName: "user3",
+    lastName: "lastname3",
+    city: "city3",
+    state: "state3",
+    county: "county3"
+},
+{
+    username: "user4",
+    password: "pass4" ,
+    email: "user4@smartag.com",
+    userType: "farmer",
+    firstName: "user4",
+    lastName: "lastname4",
+    city: "city4",
+    state: "state4",
+    county: "county4"
+},
+{
+    username: "user5",
+    password: "pass5" ,
+    email: "user5@smartag.com",
+    userType: "farmer",
+    firstName: "user5",
+    lastName: "lastname5",
+    city: "city5",
+    state: "state5",
+    county: "county5"
+},
+{
+    username: "user6",
+    password: "pass6" ,
+    email: "user6@smartag.com",
+    userType: "farmer",
+    firstName: "user6",
+    lastName: "lastname6",
+    city: "city6",
+    state: "state6",
+    county: "county6"
+},
+{
+    username: "user7",
+    password: "pass7" ,
+    email: "user7@smartag.com",
+    userType: "iot_support",
+    firstName: "user7",
+    lastName: "lastname7",
+    city: "city7",
+    state: "state7",
+    county: "county7"
+},
+{
+    username: "user8",
+    password: "pass8" ,
+    email: "user8@smartag.com",
+    userType: "farmer",
+    firstName: "user8",
+    lastName: "lastname8",
+    city: "city8",
+    state: "state8",
+    county: "county8"
+},
+{
+    username: "user9",
+    password: "pass9" ,
+    email: "user9@smartag.com",
+    userType: "farmer",
+    firstName: "user9",
+    lastName: "lastname9",
+    city: "city9",
+    state: "state9",
+    county: "county9"
+},
+{
+    username: "user10",
+    password: "pass10" ,
+    email: "user10@smartag.com",
+    userType: "infrastructure_manager",
+    firstName: "user10",
+    lastName: "lastname10",
+    city: "city10",
+    state: "state10",
+    county: "county10"
+}
+]
