69ede11129174600bf3f19825dddb4acc782720b
wenyanhe-sjsu <laksmikameswari.nishthala@sjsu.edu>
2019-04-24 03:56:53 -0700

    sensor test data ongoing

--- SmartAgCloud/Backend/test_data/sensor_test_data.js
+++ SmartAgCloud/Backend/test_data/sensor_test_data.js
@@ -1,21 +1,112 @@
-[{
-    sensorType: "humidity sensor",
+[
+{
+    sensorType: "humidity",
     status: "on",
-    x_coordinate: 2.13,
-    y_coordinate: 4.50,
+    x_coordinate: 34,
+    y_coordinate: 8,
     data: {
         timestamp: ISODate("2018-06-30T00:00:01Z"),
-        type: "humidity or temperature or blah",
-        value: 3.125
-      },
-},{
-    sensorType: "humidity sensor",
+        // type: "humidity",
+        value: 12.23
+    },
+},
+{
+    sensorType: "humidity",
     status: "on",
-    x_coordinate: 2.13,
-    y_coordinate: 4.50,
+    x_coordinate: 26,
+    y_coordinate: 73,
     data: {
-        timestamp: ISODate("2018-06-30T00:00:01Z"),
-        type: "humidity or temperature or blah",
-        value: 3.125
-}]
-
+        timestamp: ISODate("2018-06-30T01:00:01Z"),
+        // type: "humidity",
+        value: 21.59
+    }
+},
+{
+    sensorType: "humidity",
+    status: "off",
+    x_coordinate: 372,
+    y_coordinate: 434,
+    data: {
+        timestamp: ISODate("2018-06-30T02:00:01Z"),
+        // type: "humidity",
+        value: null
+    },
+},
+{
+    sensorType: "humidity",
+    status: "off",
+    x_coordinate: 33,
+    y_coordinate: 7,
+    data: {
+        timestamp: ISODate("2018-06-30T05:00:01Z"),
+        // type: "humidity",
+        value: null
+    },
+},
+{
+    sensorType: "humidity",
+    status: "on",
+    x_coordinate: 43,
+    y_coordinate: 923,
+    data: {
+        timestamp: ISODate("2018-06-30T20:00:01Z"),
+        // type: "humidity",
+        value: 8.27
+    },
+},
+{
+    sensorType: "humidity",
+    status: "maintenance",
+    x_coordinate: 294,
+    y_coordinate: 78,
+    data: {
+        timestamp: ISODate("2018-06-30T07:00:01Z"),
+        // type: "humidity",
+        value: null
+    },
+},
+{
+    sensorType: "humidity",
+    status: "off",
+    x_coordinate: 22,
+    y_coordinate: 75,
+    data: {
+        timestamp: ISODate("2018-06-30T08:00:01Z"),
+        // type: "humidity",
+        value: null
+    },
+},
+{
+    sensorType: "humidity",
+    status: "on",
+    x_coordinate: 397,
+    y_coordinate: 42,
+    data: {
+        timestamp: ISODate("2018-06-30T10:00:01Z"),
+        // type: "humidity",
+        value: 23.29
+    },
+},
+{
+    sensorType: "humidity",
+    status: "on",
+    x_coordinate: 57,
+    y_coordinate: 23,
+    data: {
+        timestamp: ISODate("2018-06-30T12:00:01Z"),
+        // type: "humidity",
+        value: 19.23
+    },
+},
+{
+    sensorType: "humidity",
+    status: "on",
+    x_coordinate: 23,
+    y_coordinate: 83,
+    data: {
+        timestamp: ISODate("2018-06-30T14:00:01Z"),
+        // type: "humidity",
+        value: 15.87
+    },
+},
+]
