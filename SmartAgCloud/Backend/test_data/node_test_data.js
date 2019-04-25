3e56931d54ca6c807e1c72f770abaaf1a99c5eee
wenyanhe-sjsu <laksmikameswari.nishthala@sjsu.edu>
2019-04-25 13:05:33 -0700

    node test data added

---
+++ SmartAgCloud/Backend/test_data/node_test_data.js
@@ -0,0 +1,112 @@
+[
+{
+    status: "on",
+    x_coordinate: 54,
+    y_coordinate: 18,
+    farmerID: 1,
+    sensor1_vacancy: false,
+    sensor2_vacancy: false,
+    sensor3_vacancy: true,
+    sensor4_vacancy: false,
+    ID: 1
+},
+{
+    status: "on",
+    x_coordinate: 78,
+    y_coordinate: 38,
+    farmerID: 1,
+    sensor1_vacancy: false,
+    sensor2_vacancy: false,
+    sensor3_vacancy: false,
+    sensor4_vacancy: false,
+    ID: 2
+},
+{
+    status: "on",
+    x_coordinate: 232,
+    y_coordinate: 93,
+    farmerID: 2,
+    sensor1_vacancy: false,
+    sensor2_vacancy: true,
+    sensor3_vacancy: true,
+    sensor4_vacancy: false,
+    ID: 3
+},
+{
+    status: "on",
+    x_coordinate: 92,
+    y_coordinate: 20,
+    farmerID: 3,
+    sensor1_vacancy: true,
+    sensor2_vacancy: false,
+    sensor3_vacancy: true,
+    sensor4_vacancy: false,
+    ID: 4
+},
+{
+    status: "on",
+    x_coordinate: 824,
+    y_coordinate: 82,
+    farmerID: 2,
+    sensor1_vacancy: true,
+    sensor2_vacancy: true,
+    sensor3_vacancy: true,
+    sensor4_vacancy: false,
+    ID: 5
+},
+{
+    status: "on",
+    x_coordinate: 28,
+    y_coordinate: 91,
+    farmerID: 1,
+    sensor1_vacancy: false,
+    sensor2_vacancy: false,
+    sensor3_vacancy: false,
+    sensor4_vacancy: false,
+    ID: 6
+},
+{
+    status: "on",
+    x_coordinate: 493,
+    y_coordinate: 187,
+    farmerID: 3,
+    sensor1_vacancy: false,
+    sensor2_vacancy: false,
+    sensor3_vacancy: true,
+    sensor4_vacancy: true,
+    ID: 7
+},
+{
+    status: "on",
+    x_coordinate: 29,
+    y_coordinate: 84,
+    farmerID: 3,
+    sensor1_vacancy: false,
+    sensor2_vacancy: false,
+    sensor3_vacancy: false,
+    sensor4_vacancy: false,
+    ID: 8
+},
+{
+    status: "on",
+    x_coordinate: 74,
+    y_coordinate: 223,
+    farmerID: 2,
+    sensor1_vacancy: true,
+    sensor2_vacancy: false,
+    sensor3_vacancy: true,
+    sensor4_vacancy: true,
+    ID: 9
+},
+{
+    status: "on",
+    x_coordinate: 542,
+    y_coordinate: 180,
+    farmerID: 2,
+    sensor1_vacancy: true,
+    sensor2_vacancy: false,
+    sensor3_vacancy: true,
+    sensor4_vacancy: true,
+    ID: 10
+}
+]
